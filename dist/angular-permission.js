ca5d9c38b6c340fae5180fa87b0b7160857162f6
chaaya <elch@ELCHAI02.momentum.se>
2015-07-08 11:30:50 +0200

    New build

--- dist/angular-permission.js
+++ dist/angular-permission.js
@@ -1,7 +1,7 @@
 /**
  * angular-permission
  * Route permission and access control as simple as it can get
- * @version v0.3.0 - 2015-07-07
+ * @version v0.3.0 - 2015-07-08
  * @link http://www.rafaelvidaurre.com
  * @author Rafael Vidaurre <narzerus@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -80,80 +80,6 @@
         }
       });
     }]);
-}());
-
-(function () {
-  'use strict';
-
-  var directives = {
-    only: 'rpOnly',
-    except: 'rpExcept'
-  };
-
-  var EXCEPTIONS = {
-      UNDEFINED_RP_BEHAVIOR: 'Specified behavior cannot be applied. Please check your spelling in the pe-behavior attribute'
-  }
-
-  angular.module('permission')
-    .directive(directives.only, only)
-
-  angular.module('permission')
-    .directive(directives.except, except)
-
-  only.$inject = ['$q', 'Permission'];
-  except.$inject = ['$q', 'Permission'];
-
-  function only($q, Permission) {
-    var directive = {
-      restrict: 'A',
-      link: link
-    }
-    return directive;
-
-    function link(scope, element, attrs) {
-      checkPermissions(directives.only, element, attrs, Permission);
-    }
-  }
-
-  function except($q, Permission) {
-    var directive = {
-      restrict: 'A',
-      link: link
-    }
-    return directive;
-
-    function link(scope, element, attrs) {
-      checkPermissions(directives.except, element, attrs, Permission);
-    }
-  }
-
-  function checkPermissions(directiveName, element, attrs, Permission) {
-    var roleMap = {};
-    var roles = attrs[directiveName].replace(/\[|]|'/gi, '').split(',');
-    roleMap[(directiveName === directives.only ? 'only' : 'except')] = roles;
-
-    var behavior = (attrs.rpBehavior ? attrs.rpBehavior : 'hide');
-    var authorizing = Permission.authorize(roleMap, null);
-    authorizing.then(null, function() {
-      applyElementBehavior(element, behavior);
-    });
-  }
-
-  function applyElementBehavior(element, behavior) {
-    switch(behavior) {
-      case 'hide':
-        element.addClass('ng-hide');
-        break;
-
-      case 'disable':
-        element.attr('disabled', 'disabled');
-        break;
-
-      default:
-        throw new Error(EXCEPTIONS.UNDEFINED_RP_BEHAVIOR);
-      }
-  }
-
 }());
 
 (function () {
